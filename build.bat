2689573d6e83af0d499c50f4eb6909fe4be9bbec
rcmdnk <rcmdnk@gmail.com>
2024-09-29 21:50:12 +0900

    chore: update AHKPath to point to AutoHotkey v2

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-SET AHKPath=C:\Program Files\AutoHotkey
+SET AHKPath=C:\Program Files\AutoHotkey\v2
 REM Takes a simple optional command parameter, /t, which starts testing before build.
 SET Param1=%1
 
