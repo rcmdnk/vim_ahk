3c6b1c8a20aaeb86b1f142a9c7b12396fb7ea29a
rcmdnk <rcmdnk@gmail.com>
2024-12-10 19:20:35 +0900

    chore: update GitHub Actions workflow to refine build and release process chore: add vim.exe to .gitignore fix: simplify and improve error handling in build.bat script

--- build.bat
+++ build.bat
@@ -4,21 +4,18 @@ REM Takes a simple optional command parameter, /t, which starts testing before b
 SET Param1=%1
 
 if "%Param1%"=="/t" (
-    start /w "%AHKPath%\v2\Autohotkey.exe" vim.ahk -quiet
-)
-REM Return code from above (0 if tests all pass) is stored in %errorlevel%
-if errorlevel 1 (
-    echo   .
-    echo   .
-    echo Tests failed. Exes not built. Log contents:
-    echo   .
-    echo   .
-    type testLogs\*
-    echo   .
-    echo   .
-    exit /b %errorlevel%
+  start /w "%AHKPath%\v2\Autohotkey.exe" tests/run_vimahk_tests.ahk -quiet
+  REM Return code from above (0 if tests all pass) is stored in %errorlevel%
+  if %errorlevel% neq 0 (
+      echo Tests failed.
+      exit /b %errorlevel%
+  )
 )
 if exist vim_ahk rmdir /s /q vim_ahk
 mkdir vim_ahk
-"%AHKPath%\Compiler\Ahk2Exe.exe" /in vim.ahk /out vim_ahk\vim_ahk.exe /compress 0 /base "%AHKPath%\v2\AutoHotkey.exe"
+"%AHKPath%\Compiler\Ahk2Exe.exe" /in vim.ahk /out vim_ahk\vim_ahk.exe /compress 0 /silent /base "%AHKPath%\v2\AutoHotkey.exe"
+if %errorlevel% neq 0 (
+  echo Build failed.
+  exit /b %errorlevel%
+)
 xcopy /i /y vim_ahk_icons vim_ahk\vim_ahk_icons
