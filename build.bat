32dfafce6e23038cb71b2af28ec3c710da7bf1de
rcmdnk <rcmdnk@gmail.com>
2024-12-10 12:36:45 +0900

    chore: update GitHub Actions workflow to simplify tag extraction and release creation fix: correct AutoHotkey path references in build script style: unify capitalization of folder names in build script paths

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
-SET AHKPath=C:\Program Files\AutoHotkey\v2
+SET AHKPath=C:\Program Files\AutoHotkey
 REM Takes a simple optional command parameter, /t, which starts testing before build.
 SET Param1=%1
 
 if "%Param1%"=="/t" (
-    start /w "%AHKPath%\Autohotkey.exe" vim.ahk -quiet
+    start /w "%AHKPath%\v2\Autohotkey.exe" vim.ahk -quiet
 )
 REM Return code from above (0 if tests all pass) is stored in %errorlevel%
 if errorlevel 1 (
@@ -20,5 +20,5 @@ if errorlevel 1 (
 )
 if exist vim_ahk rmdir /s /q vim_ahk
 mkdir vim_ahk
-"%AHKPath%\compiler\ahk2exe.exe" /in vim.ahk /out vim_ahk\vim_ahk.exe /compress 0 /base "%AHKPath%\v2\AutoHotkey.exe"
+"%AHKPath%\Compiler\Ahk2Exe.exe" /in vim.ahk /out vim_ahk\vim_ahk.exe /compress 0 /base "%AHKPath%\v2\AutoHotkey.exe"
 xcopy /i /y vim_ahk_icons vim_ahk\vim_ahk_icons
